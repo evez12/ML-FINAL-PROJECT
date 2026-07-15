1dd0679aa2386937ec8bfaa54606aea19231ba76
usermedineeee <discoverandunderstand@gmail.com>
2026-07-15 19:41:11 +0400

    Implement random forest classifier

---
+++ src/bagging/__init__.py
@@ -0,0 +1,5 @@
+# Make RandomForestClassifier easy to import from src.bagging.
+from src.bagging.random_forest import RandomForestClassifier
+
+# Only this class is public here.
+__all__ = ["RandomForestClassifier"]
